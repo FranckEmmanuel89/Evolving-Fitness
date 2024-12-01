4e90a71b7b0962bf2d9ca45b93b24f7de586f383
FranckEmmanuel89 <franckemanuel1989@yahoo.it>
2024-12-02 00:44:08 +0100

    change loader.js

--- loader.js
+++ loader.js
@@ -1,8 +1,19 @@
 const loader = document.querySelector('.loader');
+const body = document.body;
+
+// Désactive le scroll au début
+body.style.overflow = "hidden";
+
 window.addEventListener('load', () => {
-  loader.style.opacity = "0"; // Transition pour masquer le loader
-  setTimeout(() => loader.style.display = "none", 500); // Supprime complètement après 500ms
-})
+  // Transition pour masquer le loader
+  loader.style.opacity = "0"; 
+
+  // Réactive le scroll après avoir caché le loader
+  setTimeout(() => {
+    loader.style.display = "none";
+    body.style.overflow = ""; // Réinitialise le scroll
+  }, 500); // Supprime complètement après 500ms
+});
 
 
   
